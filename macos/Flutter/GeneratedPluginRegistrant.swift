5af9581a8ab1afffbccf46a9e3a622a45b9e8221
alisheraxmedov <alisheraxmedov4x4@gmail.com>
2026-07-02 20:39:08 +0500

    chore(desktop): register notification plugins in generated registrants
    flutter_timezone / flutter_local_notifications pulled in the desktop plugin
    registrations regenerated by pub get.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_local_notifications
 import flutter_secure_storage_darwin
+import flutter_timezone
 import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
